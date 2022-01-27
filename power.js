6ea2492e0c4fac97f091c6d40ac85ab3e097a51f
Pasha <dyachukpasha605@gmail.com>
2022-01-27 15:14:41 +0200

    first solution

--- power.js
+++ power.js
@@ -1,42 +1,8 @@
-'use strict'
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
-// let lastFilm = prompt('Один из просмотренных фильмов?', '')
-// let raitingFilm = +prompt('На сколько оцените его?', '')
-
-var personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors:{},
-    genres:[],
-    privat: false
-};
-
-// let filmsAndRairing = lastFilm + ':' + raitingFilm
-// console.log(filmsAndRairing);
-
-for(let i = 0; i <= 1; i++){
-    const lastFilm = prompt('Один из просмотренных фильмов?', '')
-    const raitingFilm = +prompt('На сколько оцените его?', '')
-
-    personalMovieDB.movies[i] = lastFilm + ' : ' + raitingFilm
-    // console.log(lastFilm);
-    // console.log(raitingFilm);
-}
-
-console.log(personalMovieDB);
-// alert(`Список просмотренных фильмов и их рейтинг ${personalMovieDB.movies.last} : ${personalMovieDB.movies.raiting} бала`)
-
-// console.log(personalMovieDB);
-
-// alert(`Вы посмотрели ${numberOfFilms} фильмов`)
-
-
-
-// по уроку 
-
-
+// my
 // let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+// // let lastFilm = prompt('Один из просмотренных фильмов?', '')
+// // let raitingFilm = +prompt('На сколько оцените его?', '')
 
 // var personalMovieDB = {
 //     count: numberOfFilms,
@@ -46,14 +12,70 @@ console.log(personalMovieDB);
 //     privat: false
 // };
 
+// for(let i = 0; i <= 1; i++){
+//     const lastFilm = prompt('Один из просмотренных фильмов?', '')
+//     const raitingFilm = +prompt('На сколько оцените его?', '')
 
-// const a = prompt('Один из просмотренных фильмов?', ''),
-//       b = +prompt('На сколько оцените его?', '')
-//       c = prompt('Один из просмотренных фильмов?', ''),
-//       d = +prompt('На сколько оцените его?', '');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-
+//     personalMovieDB.movies[i] = lastFilm + ' : ' + raitingFilm
+//     // console.log(lastFilm);
+//     // console.log(raitingFilm);
+// }
 // console.log(personalMovieDB);
+
+
+// по уроку 
+//first
+
+var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+
+while(numberOfFilms < 1){
+    var numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+}
+console.log(numberOfFilms);
+
+
+
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+if(personalMovieDB.count < 10){
+    alert('Просмотрено очень мало фильмов')
+}else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    alert('Вы классический зритель')
+}else if (personalMovieDB.count > 30){
+    alert('Вы киноман')
+}else{
+    alert('Ошибка! Вводите только числа')
+};
+
+
+
+var a = prompt('Один из просмотренных фильмов?', '')
+while(a.length < 1){
+    var a = prompt('Не ври! Назови один из просмотренных фильмов?', '')
+}
+while(a.length > 50){
+    var a = prompt('Очень длинное название! Максимальное к-во символов - 50', '')
+}
+const b = +prompt('На сколько оцените его?', '')
+
+var c = prompt('Еще один из просмотренных фильмов?', '')
+while(c.length < 1){
+    var c = prompt('Не ври! Назови один из просмотренных фильмов?', '')
+}
+while(c.length > 50 ){
+    var c = prompt('Очень длинное название! Максимальное к-во символов - 50', '')
+}
+const d = +prompt('На сколько оцените его?', '');
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
---
+++ powerSecond.js
@@ -0,0 +1,40 @@
+
+
+// while (numberOfFilms < 1) {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+// }
+
+while (numberOfFilms.length < 50) {
+    prompt('Максимальное к-во символов - 50')
+    numberOfFilms = +prompt('Так сколько фильмов вы уже посмотрели?', '')
+}
+
+console.log(numberOfFilms.length)
+// for (let i = 1; i < 3; i++){
+//     console.log(i) 
+// }
+
+
+
+
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
+// console.log(numberOfFilms) 
+// if (numberOfFilms <= 0) {
+//     +prompt('Сколько фильмов вы уже посмотрели?', '')
+   
+// }else if (numberOfFilms.length > 50) {
+//     +prompt('Сколько фильмов вы уже посмотрели?', '')
+// }else {
+//     console.log(numberOfFilms) 
+// }
+
+
+// let num = 50;
+
+
+
+// for (let i=1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
